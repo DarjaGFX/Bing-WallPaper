c9f1c24b0370c0e7eca3006fa84088030d642a01
Ali <ali.jafari20@gmail.com>
2017-12-10 20:58:07 +0330

    comment added

--- desktop.sh
+++ desktop.sh
@@ -7,6 +7,7 @@ cat de>>uri
 tr -d '\n' <uri >urm
 tr -d '"' <urm >ur
 cat ur | xargs -0 curl >background.jpg
+# replace your userName vs {{HostName}}
 gsettings set org.gnome.desktop.background picture-uri file:///home/{{HostName}}/background.jpg
 rm ur
 rm urm
