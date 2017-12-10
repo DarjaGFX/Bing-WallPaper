c27dc7767b77f0dc5870267d340066df376e4bc8
Ali <ali.jafari20@gmail.com>
2017-12-10 20:53:23 +0330

    set bing photo as Desktop Background :/

---
+++ desktop.sh
@@ -0,0 +1,16 @@
+curl https://www.bing.com >bing
+egrep -o -e '"/az.*jpg"' bing >dest
+head -1 dest > de
+url=https://www.bing.com
+echo $url>uri
+cat de>>uri
+tr -d '\n' <uri >urm
+tr -d '"' <urm >ur
+cat ur | xargs -0 curl >background.jpg
+gsettings set org.gnome.desktop.background picture-uri file:///home/{{HostName}}/background.jpg
+rm ur
+rm urm
+rm uri
+rm de
+rm dest
+rm bing
